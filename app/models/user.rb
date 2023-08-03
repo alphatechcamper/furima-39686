853d1e2d08d72583612efa28dfe453cad5af65c0
alphatechcamper <mizumoto2017@gmail.com>
2023-08-03 20:22:56 +0900

    userモデルのテストコード記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname,        presence: true
-         validates :last_name,       presence: true
-         validates :first_name,      presence: true
-         validates :last_name_kana,  presence: true
-         validates :first_name_kana, presence: true
-         validates :user_birth_date, presence: true
+  has_many :items
+
+  validates :nickname, presence: true
+  validates :password,
+            format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/, message: 'should be in mixed alphanumeric characters' }
+  validates :first_name, presence: true,
+                         format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'should be a full-width name' }
+  validates :last_name, presence: true,
+                        format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'should be a full-width name' }
+  validates :first_name_kana, presence: true,
+                             format: { with: /\A[ァ-ヶ]+\z/, message: 'should be a full-width katakana name' }
+  validates :last_name_kana, presence: true,
+                            format: { with: /\A[ァ-ヶ]+\z/, message: 'should be a full-width katakana name' }
+  validates :user_birth_date, presence: true
 end
