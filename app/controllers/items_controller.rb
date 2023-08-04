c722cc18ada8c722bdba2c58f7b79f43b0bd32e0
alphatechcamper <mizumoto2017@gmail.com>
2023-08-04 16:15:48 +0900

    パラメーター設定、ActiveHash導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,11 @@
 class ItemsController < ApplicationController
   def index
   end
+
+private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :info, :categpry_id, :sales_status_id, :shipping_fee_status_id,
+                                :schedule__delivery_id, :price).merge(user_id: current_user.id)
+  end
 end
