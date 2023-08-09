59a1b5168a6db451890190066731dcb7517ecf83
alphatechcamper <mizumoto2017@gmail.com>
2023-08-09 14:43:32 +0900

    ordersコントローラー記述

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,27 +1,23 @@
 class OrderForm
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, 
-                :city, :addresses, :building_name, :phone_number, :token,
-                
-   with_options presence: true do
-    validates :user_id
-    validates :item_id
-    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :addresses, :building_name, :phone_number, :token
+
+  with_options presence: true do
+    validates :city, :addresses, :token, :user_id, :item_id
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :city
-    validates :address
-    validates :phone_number, numericality: { only_integer: true }, length: { in: 10..11 }
-    validates :token
+    validates :phone_number,
+              format: { with: /\A\d{10,11}\z/, message: 'Please enter a half-width numerical value of 10 to 11 digits' }
   end
 
   def save
-    order = Order.create(user_id:, item_id:)
-    ShippingAddress.create(post_code:,
-                           prefecture_id:,
-                           municipalities:,
-                           address:,
-                           building:,
-                           phone_number:,
-                           order_id: order.id)
+    order = Order.create(user_id: user_id, item_id: item_id)
+    ShippingAdd.create(postal_code: postal_code, 
+                       prefecture_id: prefecture_id, 
+                       city: city, 
+                       addresses: addresses,
+                       building_name: building_name, 
+                       phone_number: phone_number, 
+                       order_id: order.id)
   end
 end
