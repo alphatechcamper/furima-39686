03bee658bc9e6f2a71b16bf9a30e7f90737a30f2
alphatechcamper <mizumoto2017@gmail.com>
2023-08-12 12:07:56 +0900

    soldカラムの追加

--- app/javascript/calcItemPrice.js
+++ app/javascript/calcItemPrice.js
@@ -3,6 +3,7 @@ window.addEventListener('load', function(){
   const priceInput = document.getElementById("item-price");
   const addTaxDom = document.getElementById("add-tax-price");
   const profitAmount = document.getElementById("profit");
+  
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
     const price = priceInput.value;
