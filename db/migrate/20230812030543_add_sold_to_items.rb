d6a3ae6d066e543381fac719739988c3bd8684e1
alphatechcamper <mizumoto2017@gmail.com>
2023-08-14 16:39:52 +0900

    デプロイ

--- db/migrate/20230812030543_add_sold_to_items.rb
+++
@@ -1,5 +0,0 @@
-class AddSoldToItems < ActiveRecord::Migration[7.0]
-  def change
-    add_column :items, :sold, :boolean, null: false
-  end
-end
--- db/migrate/20230812121245_remove_sold_from_items.rb
+++
@@ -1,5 +0,0 @@
-class RemoveSoldFromItems < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :items, :sold, :boolean, null: false
-  end
-end
