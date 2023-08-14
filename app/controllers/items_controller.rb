7ff0706cbbe15dd8c5313773f54c44d0c76b66a2
alphatechcamper <mizumoto2017@gmail.com>
2023-08-14 19:02:15 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :soldout_edit, only: [:edit]
 
   def index
