8ed5050a1717c3188f3dd518c22e803edced9635
alphatechcamper <mizumoto2017@gmail.com>
2023-08-16 18:08:56 +0900

    ja.yml追加、バリデーション日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,14 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :password,
-            format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/, message: 'should be in mixed alphanumeric characters' }
+            format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/, message: 'は半角英数字で入力してください' }
   validates :first_name, presence: true,
-                         format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'should be a full-width name' }
+                         format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角で入力してください' }
   validates :last_name, presence: true,
-                        format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'should be a full-width name' }
+                        format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角で入力してください' }
   validates :first_name_kana, presence: true,
-                              format: { with: /\A[ァ-ヶ]+\z/, message: 'should be a full-width katakana name' }
+                              format: { with: /\A[ァ-ヶ]+\z/, message: 'は全角（カナ）で入力してください' }
   validates :last_name_kana, presence: true,
-                             format: { with: /\A[ァ-ヶ]+\z/, message: 'should be a full-width katakana name' }
+                             format: { with: /\A[ァ-ヶ]+\z/, message: 'は全角（カナ）で入力してください' }
   validates :user_birth_date, presence: true
 end
